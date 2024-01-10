86cc9ae27df43ba64dca3933cf287145012f4c40
Novtra19 <refi246810@gmail.com>
2024-01-10 14:03:30 +0700

    commit 1
    commit 1

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', 'Admin | Dashboard Admin')
+
+@section('content')
+<div class="row mt-3 mb-3">
+    <div class="col-md-12">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="card-title">Selamat Datang di Halaman Dashboard Toko Tas</div>
+                <div class="card-title">Ini merupakan Website sederhana yang dibuat untuk menyelesaikan Tugas UAS</div>
+                <div class="card-title">Website dibuat M.Ridho</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
